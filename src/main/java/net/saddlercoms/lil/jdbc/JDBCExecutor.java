53f225dea36182925fbd2d2cc6865980ab39ef04
jazad136 <1129384+jazad136@users.noreply.github.com>
2022-03-11 10:12:32 -0500

    Initial Commit with pom.xml file

---
+++ src/main/java/net/saddlercoms/lil/jdbc/JDBCExecutor.java
@@ -0,0 +1,9 @@
+package net.saddlercoms.lil.jdbc;
+
+public class JDBCExecutor {
+	
+	public static void main(String... args) {
+		System.out.println("Hello Learning JDBC");
+	}
+	
+}
---
+++ src/main/java/snippet/JDBCExecutor2.java
@@ -0,0 +1,9 @@
+package snippet;
+
+public class JDBCExecutor2 {
+	public static void main(String... args) {
+		System.out.println("Hello Learning JDBC");
+	}
+		
+}
+
